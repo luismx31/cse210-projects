60cc3f03d3a421713e7d9c4d057c8e1471cd5422
Luis Moreno <luism@Luiss-MacBook-Air.local>
2025-11-11 00:02:06 -0500

    wk2

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,31 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        job1._startYear = 2010;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Developer";
+        job2._company = "Yahoo";
+        job2._startYear = 1999;
+        job2._endYear = 2024;
+
+          Job job3 = new Job();
+        job2._jobTitle = "Software D.";
+        job2._company = "Google";
+        job2._startYear = 2001;
+        job2._endYear = 2025;
+
+      
+        
     }
 }
